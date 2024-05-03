37bd1232c89d94562a54f92107164d8eefed9ade
shalva97 <shalva258@gmail.com>
2024-05-03 15:46:00 +0400

    added amper once again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,13 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
 }
-rootProject.name = "overwatch-player-search-api"
+//rootProject.name = "overwatch-player-search-api"
---
+++ src/main.kt
@@ -0,0 +1,24 @@
+//import io.ktor.client.*
+//import io.ktor.client.call.*
+//import io.ktor.client.plugins.contentnegotiation.*
+//import io.ktor.client.request.*
+//import io.ktor.client.statement.*
+//import io.ktor.serialization.kotlinx.json.*
+//
+//public suspend fun main() {
+//    val client = HttpClient {
+//        install(ContentNegotiation) {
+//            json()
+//        }
+//    }
+//    println(
+//        client.get("https://overwatch.blizzard.com/en-us/search/account-by-name/shalva/")
+//            .body<List<OverwatchPlayerDTO>>()
+//    )
+//
+//}
+
+fun main() {
+
+    println()
+}
