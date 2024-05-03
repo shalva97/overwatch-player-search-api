8729eb924b47e280255bed785b81ce99334e69e9
shalva97 <shalva258@gmail.com>
2024-05-03 22:52:32 +0400

    add functions to retrive title, avatar and namecard

--- library/src/io/github/shalva97/overwatch_player_search_api/PlayerSearch.kt
+++ library/src/io/github/shalva97/overwatch_player_search_api/PlayerSearch.kt
@@ -6,6 +6,10 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import io.github.shalva97.overwatch_player_search_api.models.OverwatchPlayerDTO
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 class PlayerSearch {
     private val client = HttpClient {
@@ -14,9 +18,35 @@ class PlayerSearch {
         }
     }
 
+    private val owData by lazy {
+        Json.parseToJsonElement(rawOwData)
+    }
+
     suspend fun searchForPlayer(name: String): List<OverwatchPlayerDTO> {
         return client.get("https://overwatch.blizzard.com/en-us/search/account-by-name/$name/")
             .body<List<OverwatchPlayerDTO>>()
+    }
+
+    fun getAvatar(id: String): String? {
+        return owData.jsonObject["avatars"]
+            ?.jsonObject?.get(id)
+            ?.jsonObject?.get("icon")
+            ?.jsonPrimitive?.content
+    }
+
+    fun getTitle(id: String): String? {
+        return owData.jsonObject["titles"]
+            ?.jsonObject?.get(id)
+            ?.jsonObject?.get("name")
+            ?.jsonObject?.get("en_US")
+            ?.jsonPrimitive?.content
+    }
+
+    fun getNamecard(id: String): String? {
+        return owData.jsonObject["namecards"]
+            ?.jsonObject?.get(id)
+            ?.jsonObject?.get("icon")
+            ?.jsonPrimitive?.content
     }
 
     fun close() {
---
+++ library/test/io/github/shalva97/overwatch_player_search_api/PlayerSearchTest.kt
@@ -0,0 +1,39 @@
+package io.github.shalva97.overwatch_player_search_api
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PlayerSearchTest {
+
+    @Test
+    fun getAvatar() {
+
+    }
+
+    @Test
+    fun getTitle() {
+        val search = PlayerSearch()
+
+        assertEquals("Haunted Shadow", search.getTitle("0x0250000000006B2E"))
+    }
+
+    @Test
+    fun getNamecard() {
+        val search = PlayerSearch()
+
+        assertEquals(
+            "https://d15f34w2p8l1cc.cloudfront.net/overwatch/7226589f015117e841d23356bc45835409c38ba8fb0d1451ca9268961fffde0f.png",
+            search.getNamecard("0x0250000000006A8D")
+        )
+    }
+
+    @Test
+    fun getIcon() {
+        val search = PlayerSearch()
+
+        assertEquals(
+            "https://d15f34w2p8l1cc.cloudfront.net/overwatch/d3e03d1b5b0b85f3cf98847cfd0a396a311a370363c46245e254ce2e3a040527.png",
+            search.getAvatar("0x0250000000004B0E")
+        )
+    }
+}
