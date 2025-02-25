dff4d3ae716418326153f59c0283c87df38b3b5e
shalva97 <shalva258@gmail.com>
2025-02-25 14:33:47 +0400

    update library and fix build failing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,18 +3,14 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-    }
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
 }
 
-plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.1")
-}
+include("examples", "library")
