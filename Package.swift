ce5981bd9141bc2902df94318f410e4ddce96776
shalva97 <shalva258@gmail.com>
2025-05-25 20:39:54 +0400

    fix wrong lib name

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Shared",
+    name: "OverwatchPlayerSearchApi",
     platforms: [
         .iOS(.v14),
     ],
     products: [
-        .library(name: "Shared", targets: ["Shared"])
+        .library(name: "OverwatchPlayerSearchApi", targets: ["OverwatchPlayerSearchApi"])
     ],
     targets: [
         .binaryTarget(
-            name: "Shared",
+            name: "OverwatchPlayerSearchApi",
             url: "https://github.com/shalva97/overwatch-player-search-api/releases/download/1.9/OverwatchPlayerSearchApi.xcframework.zip",
             checksum: "57a9556f96aaba1590897fdc28e2e1e41181a972e29dbb06804e023f26ccd65e")
     ]
